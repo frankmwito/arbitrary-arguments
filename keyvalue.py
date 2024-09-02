1e7665fda65a2a9507f305ad37630429da1cf65a
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 10:58:31 +0300

    **kwargs

--- keyvalue.py
+++ keyvalue.py
@@ -1,1 +1,9 @@
 # accept any number of keyword arguments and print out each key-value
+
+def key_value(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print(key_value(name = "franklyne",
+                city = "nairobi",
+                state = "Embakasi"))
