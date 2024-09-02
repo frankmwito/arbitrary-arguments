822ee130defab3143f0d7073893c6c549b855141
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 11:26:27 +0300

    mix of positional arguments and arbitrary arguments

--- arbitrary.py
+++ arbitrary.py
@@ -1,1 +1,12 @@
 # return a mix of positional arguments and arbitrary arguments
+
+def mix(name, *args, **kwargs):
+    nick_names = ', '.join(args)
+    print(f"Hello {name} your nick names are: {nick_names}")
+    
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+        
+print(mix("frank","my G", "you guy my guy",
+                city = "nairobi",
+                state = "Embakasi"))
