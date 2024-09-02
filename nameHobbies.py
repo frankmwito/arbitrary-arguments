81cbe8b8ddae48b76660bc4e8c4e4225d1a55868
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 11:22:13 +0300

    positional argument and any number of hobbies as *args

--- nameHobbies.py
+++ nameHobbies.py
@@ -1,6 +1,6 @@
 # function that takes a name as a positional argument and any number of hobbies as *args
 
-def name_hobbies(name = "frank", *args):
+def name_hobbies(name, *args):
     hobbies = ', '.join(args)
     print(f"Hello my name is {name} and i like {hobbies}")
     
