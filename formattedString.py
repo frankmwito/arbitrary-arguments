c6aaa7d64c5ac0ed392c4569b9f0dd3aa4836183
Frank Mwito <Mfranklyne039@gmail.com>
2024-09-02 11:39:22 +0300

    accept arbitrary keyword arguments for personal information and return formatted string

--- formattedString.py
+++ formattedString.py
@@ -1,1 +1,12 @@
-# accept arbitrary keyword arguments for personal information and return  as a formatted string
+# accept arbitrary keyword arguments for personal information and return formatted string
+
+def greet(**kwargs):
+    info = ', '.join(f"{key}: {value}" for key, value in kwargs.items())
+    print(f"Hello! Here is the information you provided: {info}")
+
+
+result = greet(name = "franklyne",
+      age = "34",
+      city = "new york")
+
+print(result)
